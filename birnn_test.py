b61f31adc20adc81dc8abae6020801227054ab23
Nishant <palln6935@gmail.com>
2021-06-02 15:53:50 +0530

    Add BiRNN testing

---
+++ birnn_test.py
@@ -0,0 +1,22 @@
+from keras.models import Model
+from keras.layers import LSTM, GRU, Input, Bidirectional
+import numpy as np
+
+T = 8
+D = 2
+M = 3
+
+X = np.random.randn(1, T, D)
+
+input_ = Input(shape=(T, D))
+rnn = Bidirectional(LSTM(M, return_state=True))
+x = rnn(input_)
+
+model = Model(inputs=input_, outputs=x)
+o1, h1, c1, h2, c2 = model.predict(X)
+
+print(f'o1 : ${o1}')
+print(f'h1 : ${h1}')
+print(f'c1 : ${c1}')
+print(f'h2 : ${h2}')
+print(f'c2 : ${c2}')
