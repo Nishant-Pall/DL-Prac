f8791627b249e49d69277fd02597f162f2e07adf
Nishant-Pall <palln6935@gmail.com>
2021-06-23 20:17:51 +0530

    Params to loss function

--- Custom-Models/custom_loss.py
+++ Custom-Models/custom_loss.py
@@ -11,14 +11,29 @@
 import tensorflow as tf
 
 
-def my_huber_loss(y_true, y_pred):
-    threshold = 1
-    error = y_true - y_pred
-    is_small_error = tf.abs(error) <= threshold
-    small_error_loss = tf.square(error) / 2
-    big_error_loss = threshold * (tf.abs(error) - (0.5 * threshold))
-#     		  Boolean to check   value if True     value if False
-    return tf.where(is_small_error, small_error_loss, big_error_loss)
+# def my_huber_loss(y_true, y_pred):
+#     threshold = 1
+#     error = y_true - y_pred
+#     is_small_error = tf.abs(error) <= threshold
+#     small_error_loss = tf.square(error) / 2
+#     big_error_loss = threshold * (tf.abs(error) - (0.5 * threshold))
+# #     		  Boolean to check   value if True     value if False
+#     return tf.where(is_small_error, small_error_loss, big_error_loss)
 
 
 # model.compile(optmizer='sgd', loss=my_huber_loss)
+
+# we use wrapper because the model expects loss to only accept y_true,
+# and y_pred. wrapper helps in passing more hyperparameters
+def my_huber_loss_with_threshold(threshold):
+    def my_huber_loss(y_true, y_pred):
+        error = y_true - y_pred
+        is_small_error = tf.abs(error) <= threshold
+        small_error_loss = tf.square(error) / 2
+        big_error_loss = threshold * (tf.abs(error) - (0.5 * threshold))
+        #     		  Boolean to check   value if True     value if False
+        return tf.where(is_small_error, small_error_loss, big_error_loss)
+    return my_huber_loss
+
+
+# model.compile(optmizer='sgd', loss=my_huber_loss_with_threshold(threshold=1))
