4f8ff479b4b39b04c03accf2ee3a3de5e1d60442
Nishant-Pall <palln6935@gmail.com>
2021-06-23 20:38:29 +0530

    Add Huber object loss

--- Custom-Models/custom_loss.py
+++ Custom-Models/custom_loss.py
@@ -8,6 +8,7 @@
 # from tensorflow.keras.losses import mean_squared_error
 # model.compile(loss=mean_squared_error(param=value), optmizer='sgd')
 
+from tensorflow.keras.losses import Loss
 import tensorflow as tf
 
 
@@ -37,3 +38,25 @@ def my_huber_loss_with_threshold(threshold):
 
 
 # model.compile(optmizer='sgd', loss=my_huber_loss_with_threshold(threshold=1))
+
+# Inheriting from Loss class
+class MyHuberLoss(Loss):
+    # define threshold as class value and give it initial value
+    threshold = 1
+# initializes
+
+    def __init__(self,  threshold):
+        super().__init__()
+        self.threshold = threshold
+
+# call function is executed when object is instantiated from the class
+
+    def __call__(self, y_true, y_pred):
+        error = y_true - y_pred
+        is_small_error = tf.abs(error) <= self.threshold
+        small_error_loss = tf.square(error) / 2
+        big_error_loss = self.threshold * \
+            (tf.abs(error) - (0.5 * self.threshold))
+        return tf.where(is_small_error, small_error_loss, big_error_loss)
+
+# model.compile(optmizer='sgd', loss=MyHuberLoss(threshold=1))
