ade4002571ba82f375a3d8088310282c7651f2d3
Nishant-Pall <palln6935@gmail.com>
2021-06-29 23:51:15 +0530

    Add activation to custom layer

--- Custom-Models/Custom_layers.py
+++ Custom-Models/Custom_layers.py
@@ -3,9 +3,10 @@ from tensorflow.keras.layers import Layer
 
 
 class SimpleDense(Layer):
-    def __init__(self, units=32):
+    def __init__(self, units=32, activation=None):
         super(SimpleDense, self).__init__()
         self.units = units
+        self.activation = tf.keras.activations.get(activation)
 
     def build(self, input_shape):
         w_init = tf.random_normal_initializer()
@@ -15,9 +16,10 @@ class SimpleDense(Layer):
         b_init = tf.zeros_initializer()
         self.b = tf.Variable(name='bias', initial_value=b_init(shape=(
             (self.units,)), dtype='float32'), trainable=True)
+        super().build(input_shape)
 
     def call(self, inputs):
-        return tf.matmul(inputs, self.w) + self.b
+        return self.activation(tf.matmul(inputs, self.w) + self.b)
 
 
 my_dense = SimpleDense(1)
