b1c47a357b3f3803c51e6f50cd465063be30f4b2
prhyme <palln6935@gmail.com>
2022-08-24 20:04:31 +0530

    Add spaCy

--- Torch-Prac/Torch-Text/torch_text.py
+++ Torch-Prac/Torch-Text/torch_text.py
@@ -3,9 +3,13 @@
 # Construct an iterator to do batching and padding using BucketIterator
 
 from torchtext.data import Field, TabularDataset, BucketIterator
+import spacy
+
+spacy_en = spacy.load('en_core_web_sm')
 
 
-def tokenize(x): return x.split()
+def tokenize(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
 
 
 quote = Field(sequential=True, use_vocab=True, tokenize=tokenize, lower=True)
@@ -18,30 +22,22 @@ fields = {
 train_data, test_data = TabularDataset.splits(
     path='.',
     train='train.json',
+    #     train='train.csv',
+    #     train='train.tsv',
     test='test.json',
+    #     test='test.csv',
+    #     test='test.tsv',
     # validate = 'validation.json',
     format='json',
+    #     format='csv',
+    #     format='tsv',
     fields=fields
 )
 
-# train_data, test_data = TabularDataset.splits(
-#     path='data',
-#     train='train.csv',
-#     test='test.csv',
-#     format='csv',
-#     fields=fields
-# )
-
-# train_data, test_data = TabularDataset.splits(
-#     path='data',
-#     train='train.tsv',
-#     test='test.tsv',
-#     format='tsv',
-#     fields=fields
-# )
-
-
-quote.build_vocab(train_data, max_size=10000, min_freq=1)
+quote.build_vocab(train_data, max_size=10000,
+                  min_freq=1,
+                  #  vectors='gLove.6B.100d'
+                  )
 
 train_iterator, test_iterator = BucketIterator.splits(
     (train_data, test_data),
