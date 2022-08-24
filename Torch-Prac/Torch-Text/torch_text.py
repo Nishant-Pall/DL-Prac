fff72ea63c0356953c977e56c2b63ebec29e34f3
prhyme <palln6935@gmail.com>
2022-08-24 19:51:16 +0530

    Create vocab and iterator

--- Torch-Prac/Torch-Text/torch_text.py
+++ Torch-Prac/Torch-Text/torch_text.py
@@ -40,5 +40,10 @@ train_data, test_data = TabularDataset.splits(
 #     fields=fields
 # )
 
-print(train_data[0].__dict__.keys())
-print(train_data[0].__dict__.values())
+
+quote.build_vocab(train_data, max_size=10000, min_freq=1)
+
+train_iterator, test_iterator = BucketIterator.splits(
+    (train_data, test_data),
+    batch_size=2
+)
