0e0337e6c55f5a7f1a53684d7e06b00e76777ef8
prhyme <palln6935@gmail.com>
2022-09-01 11:04:29 +0530

    Build dataset class

---
+++ Torch-Prac/Custom-data/loader.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+import spacy
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+
+
+# we want to convert text --> numerical values
+# we need a vocab mapping each word to an index
+# we need to setup a pytorch dataset to load the data
+# setup padding of every batch (all examples should be of same seq_len and setup dataloader)
+
+
+class FlickrDataset(Dataset):
+    def __init__(self, root_dir, captions_file, transform=None, freq_threshold=5):
+        self.root_dir = root_dir
+        self.df = pd.read_csv(captions_file)
+        self.transform = transform
+
+        # get img, captions column
+        self.imgs = self.df["image"]
+        self.captions = self.df["caption"]
+
+        # initialize vocab and build vocab
+        self.vocab = Vocabulary(freq_threshold)
+        self.vocab.build_vocabulary(self.captions.tolist())
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        caption = self.captions[index]
+        img_id = self.imgs[index]
+        img = Image.open(os.path.join(self.root_dir, img_id)).convert("RGB")
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        numericalized_caption = [self.vocab.stoi["<SOS>"]]
+        numericalized_caption += self.vocab.numericalize(caption)
+        numericalized_caption.append(self.vocab.stoi["<EOS>"])
+
+        return img, torch.tensor(numericalized_caption)
